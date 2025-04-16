c942874902045f8adcd5489577633f9923678713
Viroshio <anandviraj30@gmail.com>
2025-04-16 21:55:35 +0530

    added correct path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import "C:/Users/reena/Desktop/three/color-picker/src/index.css"
+import "color-picker/src/index.css"
 
 const App = () => {
   return (
